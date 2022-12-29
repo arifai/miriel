2b1192d5a8ca10877ebd3f443c3c1bd64a95268d
Ahmad Rifa'i <arifai209@gmail.com>
2022-12-29 23:50:40 +0700

    Feat: prompt

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,37 +2,48 @@ import { Telegraf } from "telegraf";
 import { config } from "../config";
 import { Model } from "../helpers/enums";
 import { Message } from "telegraf/typings/core/types/typegram";
+import { prompt } from "../helpers/prompt";
 
 
 export function setup(bot: Telegraf) {
-    bot.start((ctx) => ctx.reply("Hi 👋"))
+    bot.start((ctx) => {
+        ctx.sendChatAction("typing")
+        ctx.reply("Hi 👋")
+    })
 
     bot.on("message", async (ctx) => {
+        ctx.sendChatAction("typing")
+
         try {
             const TEMPERATURE = 0.9
-            const MAX_TOKENS = 256
+            const MAX_TOKENS = 2048
             const TOP_P = 1
             const FREQUENCY_PENALTY = 0.0
             const PRESENCE_PENALY = 0.6
-            const MESSAGE = `${(ctx.message as Message.TextMessage).text}`
+            const FULL_NAME = `${ctx.message.from.first_name} ${ctx.message.from.last_name}`
+            const PROMPT = `${prompt.response}${FULL_NAME}\nHuman:${(ctx.message as Message.TextMessage).text}\nAI:`
 
             const resp = await config.openai.createCompletion({
                 model: Model.TextDavinci003,
-                prompt: MESSAGE,
+                prompt: PROMPT,
                 temperature: TEMPERATURE,
                 max_tokens: MAX_TOKENS,
                 top_p: TOP_P,
                 frequency_penalty: FREQUENCY_PENALTY,
                 presence_penalty: PRESENCE_PENALY,
-            }, { timeout: 10000 })
+                stop: ["Human:", "AI:"]
+            }, { timeout: 15000 })
 
-            ctx.reply(`${resp.data.choices[0].text}`)
+            resp.data.choices?.forEach(e => {
+                ctx.reply(`${e.text}`)
+            })
+
         } catch (err) {
             if (err.response) {
-                console.error(`Error status`, err.response.status);
-                console.error(`Error data`, err.response.data);
+                console.error(`Error status:`, err.response.status);
+                console.error(`Error data:`, err.response.data);
             } else {
-                console.log('Unexpected error', err.message);
+                console.log('Unexpected error:', err.message);
 
             }
         }
---
+++ src/helpers/prompt.ts
@@ -0,0 +1,3 @@
+export const prompt = {
+    response: "The following is a conversation with an AI assistant. The assistant is helpful, creative, smart, interactive, clever, very friendly, the AI name is Miriel and the human name is "
+}
